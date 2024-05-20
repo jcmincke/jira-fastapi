8a55859eee97bb9cbea6507e3425612de20e2206
Jean-Christophe Mincke <jeanchristophe.mincke@gmail.com>
2024-05-20 22:34:29 +0200

    add webhook

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi import (FastAPI, Depends, Request, HTTPException, BackgroundTasks)
 
 app = FastAPI()
 
@@ -14,4 +15,13 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-    
+@app.get("/webhooks/new_ticket")
+async def new_ticket_handler(req: Request):
+
+    b = await req.body()
+    print(b)
+
+    return "hello"
+
+
+# 127.0.0.1:8000/webhooks/new_ticket
